2f68d748b1562bf3b0cd64ecd45539bff92bccd0
AntonSmagliy <mxel98@gmail.com>
2018-10-17 22:42:18 +0300

    1

---
+++ js/common.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('.sliders').slick({
+  	prevArrow: '<i class="fa fa-chevron-left" aria-hidden="true"></i>',
+	nextArrow: '<i class="fa fa-chevron-right" aria-hidden="true"></i>'
+  });
+});
+	
